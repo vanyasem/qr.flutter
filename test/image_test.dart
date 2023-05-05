de46d613413033775855c84c6b0dc29016ba4420
Ivan Semkin <ivan@semkin.ru>
2023-05-05 13:36:51 +0300

    Remove unused code

--- test/image_test.dart
+++ test/image_test.dart
@@ -209,10 +209,3 @@ Future<void> pumpWidgetWithImages(
   );
   await precacheFuture;
 }
-
-Widget buildTestableWidget(Widget widget) {
-  return MediaQuery(
-    data: const MediaQueryData(),
-    child: MaterialApp(home: widget),
-  );
-}
